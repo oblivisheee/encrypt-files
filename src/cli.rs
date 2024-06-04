5e1d031a322f30a07c302c7eec99a07543f94f75
oblivisheee <molniya213y@gmail.com>
2024-06-05 02:47:28 +0300

    Improved safety.

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,7 @@ use crate::file::*;
 use crate::utils::read_line;
 use std::path::Path;
 use std::process::Command;
+use zeroize::{Zeroize, Zeroizing};
 pub struct Interface;
 
 impl Interface {
@@ -24,14 +25,20 @@ impl Interface {
         let file_path = read_line(Some("Write file path: ")).unwrap();
         if !Path::new(&file_path).exists() {
             println!("File not found.");
+            self.wait_for_enter();
             return;
         }
-        let key = generate_aes_key();
+        let mut key = generate_aes_key();
         let file_data = load_file(&file_path).unwrap();
-        let encrypted = encrypt(&file_data, &key);
-        save_file(&file_path, &encrypted).unwrap();
+        let encrypted = encrypt(&file_data, &key.as_slice());
+        if let Err(e) = save_file(&file_path, &encrypted) {
+            println!("Error saving encrypted file: {}", e);
+            self.wait_for_enter();
+            return;
+        }
         self.clear();
         println!("Encrypted successfully.\n Your key: {}", hex::encode(&key));
+        key.zeroize();
         self.wait_for_enter()
     }
     fn decrypt_file(&self) {
@@ -39,13 +46,24 @@ impl Interface {
         let file_path = read_line(Some("Write file path: ")).unwrap();
         if !Path::new(&file_path).exists() {
             println!("File not found.");
+            self.wait_for_enter();
             return;
         }
-        let key = read_line(Some("Write key: ")).unwrap();
-        let key_bytes = hex::decode(key).unwrap();
+        let mut key = read_line(Some("Write key: ")).unwrap();
+        if key.len() != 64 {
+            println!("Invalid key length. Key must be 64 characters long.");
+            self.wait_for_enter();
+            return;
+        }
+        let key_bytes = Zeroizing::new(hex::decode(key.clone()).unwrap());
+        key.zeroize();
         let file_data = load_file(&file_path).unwrap();
         let decrypted = decrypt(&file_data, &key_bytes);
-        save_file(&file_path, &decrypted).unwrap();
+        if let Err(e) = save_file(&file_path, &decrypted) {
+            println!("Error saving decrypted file: {}", e);
+            self.wait_for_enter();
+            return;
+        }
         self.clear();
         println!("Decrypted successfully.");
         self.wait_for_enter()
--- src/crypto.rs
+++ src/crypto.rs
@@ -1,5 +1,6 @@
 use rand::Rng;
 use soft_aes::aes::{aes_dec_ecb, aes_enc_ecb};
+use zeroize::Zeroizing;
 
 pub fn encrypt(plaintext: &[u8], key: &[u8]) -> Vec<u8> {
     aes_enc_ecb(plaintext, key, Some("PKCS7")).unwrap()
@@ -9,8 +10,8 @@ pub fn decrypt(ciphertext: &[u8], key: &[u8]) -> Vec<u8> {
     aes_dec_ecb(ciphertext, key, Some("PKCS7")).unwrap()
 }
 
-pub fn generate_aes_key() -> [u8; 32] {
+pub fn generate_aes_key() -> Zeroizing<[u8; 32]> {
     let mut key = [0u8; 32];
     rand::thread_rng().fill(&mut key);
-    key
+    Zeroizing::new(key)
 }
